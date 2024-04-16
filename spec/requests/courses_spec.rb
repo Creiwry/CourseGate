7ca2276c788804fa34dc864696b0b87758c2b702
Creiwry <jay.gardos@outlook.com>
2024-04-16 19:59:37 +0200

    fix(tests): fix courses request spec

--- spec/requests/courses_spec.rb
+++ spec/requests/courses_spec.rb
@@ -10,21 +10,21 @@ RSpec.describe "Courses", type: :request do
 
   describe 'GET #index' do
     it 'returns a success response' do
-      get :index
+      get courses_path
       expect(response).to be_successful
     end
   end
 
   describe 'GET #show' do
     it 'returns a success response' do
-      get :show, params: { id: course.id }
+      get course_path(course.id)
       expect(response).to be_successful
     end
   end
 
   describe 'GET #new' do
     it 'returns a success response' do
-      get :new
+      get new_course_path
       expect(response).to be_successful
     end
   end
@@ -32,19 +32,19 @@ RSpec.describe "Courses", type: :request do
   describe 'POST #create' do
     it 'creates a new course' do
       expect do
-        post :create, params: { course: attributes_for(:course) }
+        post courses_path, params: { course: attributes_for(:course) }
       end.to change(Course, :count).by(1)
     end
 
     it 'redirects to the created course' do
-      post :create, params: { course: attributes_for(:course) }
+      post courses_path, params: { course: attributes_for(:course) }
       expect(response).to redirect_to(Course.last)
     end
   end
 
   describe 'GET #edit' do
     it 'returns a success response' do
-      get :edit, params: { id: course.id }
+      get edit_course_path(course.id)
       expect(response).to be_successful
     end
   end
@@ -52,13 +52,14 @@ RSpec.describe "Courses", type: :request do
   describe 'PATCH #update' do
     it 'updates the course' do
       new_name = 'New Course Name'
-      patch :update, params: { id: course.id, course: { name: new_name } }
+      patch course_path(course.id), params: { id: course.id, course: { name: new_name } }
       course.reload
       expect(course.name).to eq(new_name)
     end
 
     it 'redirects to the course' do
-      patch :update, params: { id: course.id, course: attributes_for(:course) }
+      new_name = 'New Course Name'
+      patch course_path(course.id), params: { id: course.id, course: { name: new_name } }
       expect(response).to redirect_to(course)
     end
   end
@@ -67,12 +68,12 @@ RSpec.describe "Courses", type: :request do
     it 'destroys the course' do
       course
       expect do
-        delete :destroy, params: { id: course.id }
+        delete course_path(course.id)
       end.to change(Course, :count).by(-1)
     end
 
     it 'redirects to the courses list' do
-      delete :destroy, params: { id: course.id }
+      delete course_path(course.id)
       expect(response).to redirect_to(courses_url)
     end
   end
--- spec/requests/static_spec.rb
+++ spec/requests/static_spec.rb
@@ -1,7 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe "Statics", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "GET /about_us" do
+    it 'returns a success' do
+      get about_us_path
+      expect(response).to be_successful
+    end
+  end
+
+  describe "GET /faq" do
+    it 'returns a success' do
+      get faq_path
+      expect(response).to be_successful
+    end
   end
 end
