75eb525d7e7e049ac958c34c67545ebfd753c322
Creiwry <jay.gardos@outlook.com>
2024-04-16 20:31:59 +0200

    fix(tests): fix forum threads request spec

--- spec/requests/forum_threads_spec.rb
+++ spec/requests/forum_threads_spec.rb
@@ -8,14 +8,14 @@ RSpec.describe "ForumThreads", type: :request do
 
   describe 'GET #index' do
     it 'returns a success response' do
-      get :index
+      get forum_threads_path
       expect(response).to be_successful
     end
   end
 
   describe 'GET #show' do
     it 'returns a success response' do
-      get :show, params: { id: forum_thread.id }
+      get forum_thread_path(forum_thread.id)
       expect(response).to be_successful
     end
   end
@@ -24,8 +24,9 @@ RSpec.describe "ForumThreads", type: :request do
     before do
       sign_in(instructor)
     end
+
     it 'returns a success response' do
-      get :new
+      get new_forum_thread_path
       expect(response).to be_successful
     end
   end
@@ -34,14 +35,15 @@ RSpec.describe "ForumThreads", type: :request do
     before do
       sign_in(instructor)
     end
+
     it 'creates a new forum thread' do
       expect do
-        post :create, params: { forum_thread: attributes_for(:forum_thread, course_id: course.id) }
+        post forum_threads_path, params: { forum_thread: attributes_for(:forum_thread, course_id: course.id) }
       end.to change(ForumThread, :count).by(1)
     end
 
     it 'redirects to the created forum thread' do
-      post :create, params: { forum_thread: attributes_for(:forum_thread, course_id: course.id) }
+      post forum_threads_path, params: { forum_thread: attributes_for(:forum_thread, course_id: course.id) }
       expect(response).to redirect_to(ForumThread.last)
     end
   end
@@ -50,8 +52,9 @@ RSpec.describe "ForumThreads", type: :request do
     before do
       sign_in(instructor)
     end
+
     it 'returns a success response' do
-      get :edit, params: { id: forum_thread.id }
+      get edit_forum_thread_path(forum_thread.id)
       expect(response).to be_successful
     end
   end
@@ -60,15 +63,16 @@ RSpec.describe "ForumThreads", type: :request do
     before do
       sign_in(instructor)
     end
+
     it 'updates the forum thread' do
       new_name = 'New Forum Thread Name'
-      patch :update, params: { id: forum_thread.id, forum_thread: { title: new_name, course_id: forum_thread.course.id } }
+      patch forum_thread_path(forum_thread.id), params: { id: forum_thread.id, forum_thread: { title: new_name, course_id: forum_thread.course.id } }
       forum_thread.reload
       expect(forum_thread.title).to eq(new_name)
     end
 
     it 'redirects to the forum thread' do
-      patch :update, params: { id: forum_thread.id, forum_thread: { title: 'newer name', course_id: forum_thread.course.id } }
+      patch forum_thread_path(forum_thread.id), params: { id: forum_thread.id, forum_thread: { title: 'newer name', course_id: forum_thread.course.id } }
       expect(response).to redirect_to(forum_thread)
     end
   end
@@ -77,17 +81,18 @@ RSpec.describe "ForumThreads", type: :request do
     before do
       sign_in(instructor)
     end
+
     it 'destroys the forum thread' do
       forum_thread
 
       expect do
-        delete :destroy, params: { id: forum_thread.id, forum_thread: { course_id: forum_thread.course.id } }
+        delete forum_thread_path(forum_thread.id), params: { id: forum_thread.id, forum_thread: { course_id: forum_thread.course.id } }
       end.to change(ForumThread, :count).by(-1)
     end
 
     it 'redirects to the forum threads list' do
       forum_thread
-      delete :destroy, params: { id: forum_thread.id, forum_thread: { course_id: forum_thread.course.id } }
+      delete forum_thread_path(forum_thread.id), params: { id: forum_thread.id, forum_thread: { course_id: forum_thread.course.id } }
       expect(response).to redirect_to(forum_threads_url)
     end
   end
